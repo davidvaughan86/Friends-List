cb8e807167deadd88d2f8ef77b4f379f43330a8d
davidvaughan86 <david.vaughan86@gmail.com>
2021-05-02 19:34:04 -0400

    home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,11 @@
 class HomeController < ApplicationController
   def index
+  	
   end
 
   def about
   	@about_me = "My Name is David Vaughan, check out my portfolio @ https://www.davethedev.org"
 
-  	
+
   end
 end
