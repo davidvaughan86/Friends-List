713c7dfe085733ed5c1d22276307a4d2b6e6d816
davidvaughan86 <david.vaughan86@gmail.com>
2021-05-02 23:31:58 -0400

    about page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
-  	
+
   end
 
   def about
-  	@about_me = "My Name is David Vaughan, check out my portfolio @ https://www.davethedev.org"
+  	@about_me = "My Name is David Vaughan, check out my portfolio @"
 
 
   end
